3daba2d81010e549bac249e7b86905e7a57b91a8
Ben Croker <57572400+bencroker@users.noreply.github.com>
2022-03-12 18:59:10 +0000

    Addl → Additional

--- src/base/Element.php
+++ src/base/Element.php
@@ -320,10 +320,10 @@ abstract class Element extends Component implements ElementInterface
 
     /**
      * @event DefineHtmlEvent The event that is triggered when defining additional buttons that should be shown at the top of the element’s edit page.
-     * @see getAddlButtons()
+     * @see getAdditionalButtons()
      * @since 4.0.0
      */
-    public const EVENT_DEFINE_ADDL_BUTTONS = 'defineAddlButtons';
+    public const EVENT_DEFINE_ADDITIONAL_BUTTONS = 'defineAdditionalButtons';
 
     /**
      * @event DefineHtmlEvent The event that is triggered when defining the HTML for the editor sidebar.
@@ -2846,11 +2846,11 @@ abstract class Element extends Component implements ElementInterface
     /**
      * @inheritdoc
      */
-    public function getAddlButtons(): string
+    public function getAdditionalButtons(): string
     {
-        // Fire a defineAddlButtons event
+        // Fire a defineAdditionalButtons event
         $event = new DefineHtmlEvent();
-        $this->trigger(self::EVENT_DEFINE_ADDL_BUTTONS, $event);
+        $this->trigger(self::EVENT_DEFINE_ADDITIONAL_BUTTONS, $event);
         return $event->html;
     }
 
@@ -4329,10 +4329,10 @@ JS,
         $propSites = array_values(array_filter($supportedSites, fn($site) => $site['propagate']));
         $propSiteIds = array_column($propSites, 'siteId');
         $propEditableSiteIds = array_intersect($propSiteIds, $allEditableSiteIds);
-        $addlEditableSites = array_values(array_filter($supportedSites, fn($site) => !$site['propagate'] && in_array($site['siteId'], $allEditableSiteIds)));
+        $additionalEditableSites = array_values(array_filter($supportedSites, fn($site) => !$site['propagate'] && in_array($site['siteId'], $allEditableSiteIds)));
 
         if (count($supportedSites) > 1) {
-            $expandStatusBtn = (count($propEditableSiteIds) > 1 || $addlEditableSites)
+            $expandStatusBtn = (count($propEditableSiteIds) > 1 || $additionalEditableSites)
                 ? Html::button('', [
                     'class' => ['expand-status-btn', 'btn'],
                     'data' => [
